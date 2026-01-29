44ab2d5ba493fd3a1f84b4c47198bf1135db61bc
Loooy <1464631083@qq.com>
2026-01-29 15:32:42 +0700

    新建项目；新建Readme

---
+++ Models/Server.cs
@@ -0,0 +1,19 @@
+﻿namespace LY_WebUI.Models
+{
+    public class Server
+    {
+        public Server()
+        {
+            Random rand = new Random();
+            int randomId = rand.Next(0, 2);
+            IsOnline = randomId == 1 ? true : false ;
+
+        }
+
+        public int ServerId { get; set; }
+        public bool IsOnline { get; set; }
+        public string? Name { get; set; }
+        public string? City { get; set; }
+
+    }
+}
--- Readme.md
+++ Readme.md
@@ -1,3 +1,28 @@
 ﻿# 此项目为前端学习项目 后面会调用WebAPI进行数据交互
 
 ## 项目结构
+```
+Step1  app.MapRazorComponents<App>(); 中间件
+Step2  App.razor 根组件
+Step3  Routes.razor 路由组件
+Step4  MainLayout.razor 布局组件
+Step5  Pages 组件页面/自定义组件
+```
+
+## 知识
+```
+可路由组件(必须加 @page 指令)
+
+@page "/servers"
+<h3>Servers</h3>
+@code {
+....
+}
+
+不可路由组件(没有 @page 指令，其他组件可以使用)
+
+
+全局命名空间 _imports
+
+隐式razor表达式
+显式razor表达式
